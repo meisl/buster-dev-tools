b86ef00dda45f5b834a9b91ffacd7253d98efeab
Matthias Kling <m.kling@sprax.eu>
2012-03-12 19:45:02 +0100

    * using what `npm root` says rather than %NODE_PATH% * renamed former var nodePath to npmRoot, that's what it is + checking for option -g or --global - TODO: what to do with that? * fix: when testing against modules with problematic dependencies use cwd rather than project.name... this way it works even when not invoked functions.js

--- npm-link-for-win.js
+++ npm-link-for-win.js
@@ -7,53 +7,58 @@ var ERROR_CODE_CMD_NOT_FOUND = 1;
 if (process.platform != "win32") { // what about win64 ?!
     throw new Error("this is a workaround for windows only, please use npm link instead");
 }
-var arg = process.argv[2];
-var nodePath = process.env["NODE_PATH"];
-if (!nodePath) {
-    nodePath = process.env["APPDATA"] + "\\npm\\node_modules";
-    console.log("no NODE_PATH in environment, using " + quote(nodePath) + " instead.");
+var args = process.argv.slice(2);
+var optIdx;
+if ((optIdx = args.indexOf('-g') >= 0) || (optIdx = args.indexOf('--global') >= 0)) {
+    // TODO: find out about -g option for npm link
+    throw new Error("Does npm link have a -g option? I don't know...");
 }
-nodePath += "\\";
-if (!arg) {
-    var cwd = process.cwd();
-    var moduleName = path.basename(cwd);
-    var link = nodePath + moduleName;
-    var whereLinkPointsTo = cwd;
-    if (path.existsSync(link)) {
-        console.log("skipped npm-linking " + moduleName + ", already exists in " + nodePath);
-    } else {
-        var cmdSuffix = quote(link) + " " + quote(whereLinkPointsTo);
-        var cmdJ = "junction.exe " + cmdSuffix; // for XP
-        var cmdM = "mklink /D " + cmdSuffix; // built into Vista and later
-        cp.exec(cmdM, function(err, stdout, stderr) {
-            if(err) {
-                if (err.code == ERROR_CODE_CMD_NOT_FOUND) {
-                    // so we're probably on XP, let's try junction.exe then:
-                    cp.exec(cmdJ, function(err, stdout, stderr) {
-                        if (err) {
-                            if (err.code == ERROR_CODE_CMD_NOT_FOUND) {
-                                throw new Error(cmdJ + " not found, get it from http://technet.microsoft.com/en-us/sysinternals/bb896768");
-                            } else {
-                                rethrow(err, stdout, stderr);
+if (args.length == 0) {
+    cp.exec('npm root -g', function(err, stdout, stderr) {
+        if (err) rethrow(err, stdout, stderr);
+        var npmRoot = stdout.substring(0, stdout.length-1);   // TODO: really ever only one \n on stdout??!
+        npmRoot += "\\";
+        debugger;
+        var cwd = process.cwd();
+        var moduleName = path.basename(cwd);
+        var link = npmRoot + moduleName;
+        var whereLinkPointsTo = cwd;
+        if (path.existsSync(link)) {
+            console.log("skipped npm-linking " + moduleName + ", already exists in " + npmRoot);
+        } else {
+            var cmdSuffix = quote(link) + " " + quote(whereLinkPointsTo);
+            var cmdJ = "junction.exe " + cmdSuffix; // for XP
+            var cmdM = "mklink /D " + cmdSuffix; // built into Vista and later
+            cp.exec(cmdM, function(err, stdout, stderr) {
+                if(err) {
+                    if (err.code == ERROR_CODE_CMD_NOT_FOUND) {
+                        // so we're probably on XP, let's try junction.exe then:
+                        cp.exec(cmdJ, function(err, stdout, stderr) {
+                            if (err) {
+                                if (err.code == ERROR_CODE_CMD_NOT_FOUND) {
+                                    throw new Error(cmdJ + " not found, get it from http://technet.microsoft.com/en-us/sysinternals/bb896768");
+                                } else {
+                                    rethrow(err, stdout, stderr);
+                                }
                             }
-                        }
-                    });
-                } else {
-                    rethrow(err, stdout, stderr);
+                        });
+                    } else {
+                        rethrow(err, stdout, stderr);
+                    }
                 }
-            }
-        });
-    }
-    // FIXME: for now we just skip where npm install croaks:
-    if ((project.name != "buster-syntax") // jsdom (contextify)
-        && (project.name != "buster-test") // jsdom (contextify)
-        && (project.name != "buster-html-doc") // jsdom (contextify)
-        && (project.name != "buster-bayeux-emitter") // faye (redis)
-     ) {
-        cp.exec("npm install", function(err, stdout, stderr) {
-            if (err) rethrow(err, stdout, stderr);
-        });
-     }
+            });
+        }
+        // FIXME: for now we just skip where npm install croaks:
+        if ((cwd != "buster-syntax") // jsdom (contextify)
+            && (cwd != "buster-test") // jsdom (contextify)
+            && (cwd != "buster-html-doc") // jsdom (contextify)
+            && (cwd != "buster-bayeux-emitter") // faye (redis)
+         ) {
+            cp.exec("npm install", function(err, stdout, stderr) {
+                if (err) rethrow(err, stdout, stderr);
+            });
+         }
+    });
 } else {
     throw new Error("Windows replacement for  npm link <package-name>  is not implemented");
 }
