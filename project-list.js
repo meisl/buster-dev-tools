a6500178a2326fb79216c4e4dff0d97276cbf52d
Matthias Kling <m.kling@sprax.eu>
2012-03-17 19:09:05 +0100

    + two comments in gitmodules-list.txt + leaving out dependency faye if on Windows * using https://, meisl forks for the six buster projects affected by abandon_git_submodules (TODO: what about the 7th, browser-logos)

--- project-list.js
+++ project-list.js
@@ -1,15 +1,14 @@
 var projects = [
-    {name: "buster-evented-logger", gitUrl: "https://github.com/meisl/buster-evented-logger.git", branch: "abandon_git_submodules"},
-    {name: "buster-capture-server", gitUrl: "https://github.com/meisl/buster-capture-server.git", branch: "abandon_git_submodules"}
-/*
+    {name: "buster-evented-logger", gitUrl: "https://meisl@github.com/meisl/buster-evented-logger.git", branch: "abandon_git_submodules"},
+    {name: "buster-capture-server", gitUrl: "https://meisl@github.com/meisl/buster-capture-server.git", branch: "abandon_git_submodules"},
 
-    "buster-core",
-    "buster-assertions",
+    {name: "buster-core",           gitUrl: "https://meisl@github.com/meisl/buster-core.git",           branch: "abandon_git_submodules"},
+    {name: "buster-assertions",     gitUrl: "https://meisl@github.com/meisl/buster-assertions.git",     branch: "abandon_git_submodules"},
 
-    "buster-format",
-    "buster-jstestdriver",
+    {name: "buster-format",         gitUrl: "https://meisl@github.com/meisl/buster-format.git",         branch: "abandon_git_submodules"},
+    {name: "buster-jstestdriver",   gitUrl: "https://meisl@github.com/meisl/buster-jstestdriver.git",   branch: "abandon_git_submodules"},
 
-    {name: "sinon", gitUrl: "https://github.com/cjohansen/Sinon.JS.git"},
+	{name: "sinon", gitUrl: "https://github.com/cjohansen/Sinon.JS.git"},
     "buster-util",
     "buster-user-agent-parser",
     "buster-terminal",
@@ -27,9 +26,8 @@ var projects = [
     "buster-cli",
     "buster-test-cli",
     "buster-static",
-    "buster",
-    "buster-html-doc"
-*/
+    "buster"
+//   ,"buster-html-doc" // not really necessary; depends on contextify (through jsdom) which makes problems on Win
 ];
 
 try { projects = require("./local").concat(projects) } catch(e){};
