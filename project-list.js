cd0ded8ffca4b115d6525a393aae526afe57cdc7
Matthias Kling <m.kling@sprax.eu>
2012-03-17 16:58:01 +0100

    + project list: not only repo can be specified but also branch * fetching buster-capture-server and buster-evented-logger from meisl forks, branch abandon_git_submodule * temporarily commented out rest of internal projects, for testing

--- project-list.js
+++ project-list.js
@@ -1,19 +1,24 @@
 var projects = [
+    {name: "buster-evented-logger", gitUrl: "https://github.com/meisl/buster-evented-logger.git", branch: "abandon_git_submodules"},
+    {name: "buster-capture-server", gitUrl: "https://github.com/meisl/buster-capture-server.git", branch: "abandon_git_submodules"}
+/*
+
+    "buster-core",
+    "buster-assertions",
+
+    "buster-format",
+    "buster-jstestdriver",
+
     {name: "sinon", gitUrl: "https://github.com/cjohansen/Sinon.JS.git"},
     "buster-util",
     "buster-user-agent-parser",
     "buster-terminal",
     "buster-analyzer",
     "buster-syntax",
-    "buster-core",
-    "buster-assertions",
-    "buster-format",
-    "buster-evented-logger",
     "buster-test",
     "sinon-buster",
     "buster-glob",
     "buster-resources",
-    "buster-capture-server",
     "buster-bayeux-emitter",
     "buster-configuration",
     "buster-client",
@@ -23,8 +28,8 @@ var projects = [
     "buster-test-cli",
     "buster-static",
     "buster",
-    "buster-jstestdriver",
     "buster-html-doc"
+*/
 ];
 
 try { projects = require("./local").concat(projects) } catch(e){};
